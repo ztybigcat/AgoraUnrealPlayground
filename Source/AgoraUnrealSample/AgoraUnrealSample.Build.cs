92705ccd4c838cfd43ecc31090f9eaac10d5a62f
Tianyi Zhang <zhang.tianyi9@northeastern.edu>
2024-02-20 18:33:44 -0800

    working demo app

--- Source/AgoraUnrealSample/AgoraUnrealSample.Build.cs
+++ Source/AgoraUnrealSample/AgoraUnrealSample.Build.cs
@@ -10,14 +10,19 @@ public class AgoraUnrealSample : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "AgoraUnrealSample", "AgoraPlugin" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        //if (Target.Platform == UnrealTargetPlatform.IOS || Target.Platform == UnrealTargetPlatform.Mac)
+        //{
+        //    bOverrideBuildEnvironment = true;
+        //    AdditionalCompilerArguments = "-Wno - unused - but - set - variable - Wno - gcc - compat - Wno - reorder - ctor";
+        //}
+    }
 }
